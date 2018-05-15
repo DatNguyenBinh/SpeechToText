beedfa3a6e953058631336adf47a01dee354a4bb
datnguyenbinh <binhdat240793@gmail.com>
2018-05-15 22:22:58 +0700

    Add feature speech to text

--- app/src/main/java/com/datnguyenbinh/demospeechtotext/MainActivity.java
+++ app/src/main/java/com/datnguyenbinh/demospeechtotext/MainActivity.java
@@ -1,13 +1,63 @@
 package com.datnguyenbinh.demospeechtotext;
 
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView txtSpeechInput;
+    private ImageButton btnSpeak;
+    private static final int REQ_CODE_SPEECH_INPUT = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        txtSpeechInput = findViewById(R.id.txtSpeechInput);
+
+        btnSpeak = findViewById(R.id.btn_speak);
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                promptSpeechInput();
+            }
+        });
+    }
+
+    private void promptSpeechInput(){
+        Intent recognizerIntetn = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        recognizerIntetn.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        recognizerIntetn.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        recognizerIntetn.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.speech_prompt));
+
+        startActivityForResult(recognizerIntetn, REQ_CODE_SPEECH_INPUT);
+    }
+
+    /**
+     * Receiving speech input
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case REQ_CODE_SPEECH_INPUT:
+                if (resultCode == RESULT_OK && null != data){
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtSpeechInput.setText(result.get(0));
+                }
+                break;
+        }
     }
 }
